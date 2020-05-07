7abbc9d40a3b2905517158f0310a0e3011572ccd
Thiago Souto <soutosss@gmail.com>
2020-05-07 15:23:33 -0300

    Remove setContentView from onCreate

--- app/src/main/kotlin/com/soutosss/marvelpoc/home/MainActivity.kt
+++ app/src/main/kotlin/com/soutosss/marvelpoc/home/MainActivity.kt
@@ -6,14 +6,12 @@ import com.soutosss.marvelpoc.R
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private val homeViewModel: HomeViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         lifecycle.addObserver(homeViewModel)
-
         viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
     }
 }
