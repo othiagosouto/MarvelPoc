ba5d016e0989753c520a630c3665fc27aac9f4ad
Thiago Souto <soutosss@gmail.com>
2020-05-07 22:52:12 -0300

    Remove unused imports

--- app/src/androidTest/kotlin/com/soutosss/marvelpoc/home/list/CharactersFragmenRobot.kt
+++ app/src/androidTest/kotlin/com/soutosss/marvelpoc/home/list/CharactersFragmenRobot.kt
@@ -7,10 +7,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-import com.google.gson.Gson
 import com.soutosss.marvelpoc.R
 import com.soutosss.marvelpoc.data.CharactersRepository
-import com.soutosss.marvelpoc.data.model.character.MarvelCharactersResponse
 import com.soutosss.marvelpoc.data.model.view.CharacterHome
 import com.soutosss.marvelpoc.home.HomeViewModel
 import com.soutosss.marvelpoc.shared.livedata.Result
@@ -18,9 +16,7 @@ import io.mockk.mockk
 import org.hamcrest.Matchers.not
 import org.koin.core.KoinComponent
 import org.koin.core.context.loadKoinModules
-import org.koin.core.inject
 import org.koin.dsl.module
-import java.util.concurrent.Semaphore
 
 fun configure(func: CharactersFragmentConfiguration.() -> Unit) =
     CharactersFragmentConfiguration().apply(func)
