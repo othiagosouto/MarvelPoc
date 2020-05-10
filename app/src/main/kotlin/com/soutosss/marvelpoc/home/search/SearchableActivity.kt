134d29f166d6d66bd784a6602b291375b72e2384
Thiago Souto <soutosss@gmail.com>
2020-05-10 16:43:18 -0300

    Create Searchable Activity

---
+++ app/src/main/kotlin/com/soutosss/marvelpoc/home/search/SearchableActivity.kt
@@ -0,0 +1,13 @@
+package com.soutosss.marvelpoc.home.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.soutosss.marvelpoc.R
+
+class SearchableActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_searchable)
+    }
+}
