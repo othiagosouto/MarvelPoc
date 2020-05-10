d9984bbe7cc92f7c51ff893e73f49422f60a9e3d
Thiago Souto <soutosss@gmail.com>
2020-05-10 18:41:05 -0300

    Convert test to use phrase name

--- data/src/test/java/com/soutosss/marvelpoc/data/ext/StringExtKtTest.kt
+++ data/src/test/java/com/soutosss/marvelpoc/data/ext/StringExtKtTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 class StringExtKtTest {
 
     @Test
-    fun toMD5_shouldCreateAValidMD5Hash() {
+    fun `toMD5 should transform string into a MD5 hash`() {
         assertThat("1323123d".toMD5()).isEqualTo("e2526bbcc3c041bd6d39f59bb57b4940")
     }
 }
