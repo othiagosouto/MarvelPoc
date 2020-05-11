266f9cc50ac91c975a16f91da2330ff4bcdf61eb
Thiago Souto <soutosss@gmail.com>
2020-05-11 00:25:40 -0300

    Create CharactersDataSource to fetch content from api using retrofit and paging

---
+++ data/src/main/java/com/soutosss/marvelpoc/data/CharactersDataSource.kt
@@ -0,0 +1,59 @@
+package com.soutosss.marvelpoc.data
+
+import androidx.paging.PositionalDataSource
+import com.soutosss.marvelpoc.data.local.CharacterHomeDAO
+import com.soutosss.marvelpoc.data.model.character.toCharacterHomeList
+import com.soutosss.marvelpoc.data.model.view.CharacterHome
+import com.soutosss.marvelpoc.data.network.CharactersApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class CharactersDataSource(
+    private val queryText: String?,
+    private val scope: CoroutineScope,
+    private val api: CharactersApi,
+    private val dao: CharacterHomeDAO,
+    private val exceptionHandler: (Exception) -> Unit,
+    private val loadFinished: ()  -> Unit
+) :
+    PositionalDataSource<CharacterHome>() {
+
+    override fun loadInitial(
+        params: LoadInitialParams,
+        callback: LoadInitialCallback<CharacterHome>
+    ) {
+        scope.launch {
+            try {
+                val response = api.listCharacters(name = queryText, offset = 0, limit = params.requestedLoadSize)
+                callback.onResult(response.toCharacterHomeList().checkFavorite(), 0)
+                loadFinished()
+            } catch (e: Exception) {
+                exceptionHandler(e)
+            }
+        }
+    }
+
+    private suspend fun List<CharacterHome>.checkFavorite(): List<CharacterHome> {
+        val favorites = dao.favoriteIds()
+        favorites.forEach { id ->
+            this.firstOrNull { it.id == id }?.favorite = true
+        }
+        return this
+    }
+
+    override fun loadRange(
+        params: LoadRangeParams,
+        callback: LoadRangeCallback<CharacterHome>
+    ) {
+        scope.launch {
+            try {
+                val response =
+                    api.listCharacters(name = queryText, offset = params.startPosition, limit = params.loadSize)
+                callback.onResult(response.toCharacterHomeList().checkFavorite())
+            } catch (e: Exception) {
+                exceptionHandler(e)
+            }
+        }
+    }
+}
---
+++ data/src/main/java/com/soutosss/marvelpoc/data/model/EmptyDataException.kt
@@ -0,0 +1,5 @@
+package com.soutosss.marvelpoc.data.model
+
+import java.lang.Exception
+
+class EmptyDataException : Exception()
