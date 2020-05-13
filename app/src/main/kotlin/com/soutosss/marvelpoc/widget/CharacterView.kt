8747471266ff023177620edd2ff9c15659d3d79f
Thiago Souto <soutosss@gmail.com>
2020-05-13 20:45:40 -0300

    Create CharacterView

---
+++ app/src/main/kotlin/com/soutosss/marvelpoc/widget/CharacterView.kt
@@ -0,0 +1,60 @@
+package com.soutosss.marvelpoc.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.widget.ContentLoadingProgressBar
+import com.soutosss.marvelpoc.R
+import com.soutosss.marvelpoc.data.model.view.Character
+import com.soutosss.marvelpoc.loadHomeImage
+
+class CharacterView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+    private val headerText by lazy { findViewById<TextView>(R.id.text) }
+    private val imageView by lazy { findViewById<ImageView>(R.id.image) }
+    private val progress by lazy { findViewById<ContentLoadingProgressBar>(R.id.progressItem) }
+    private val favoriteCheckBox by lazy { findViewById<CheckBox>(R.id.favorite) }
+    private lateinit var character: Character
+    private lateinit var favoriteClick: (Character) -> Unit
+    private var itemClick: ((Character) -> Unit)? = null
+
+    init {
+        val inflater: LayoutInflater =
+            context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.view_character, this, true)
+
+        favoriteCheckBox.setOnClickListener {
+            character.favorite = character.favorite.not()
+            character.let(favoriteClick)
+        }
+        this.setOnClickListener {
+            itemClick?.invoke(character)
+        }
+
+        headerText.setOnClickListener {
+            itemClick?.invoke(character)
+        }
+    }
+
+    fun updateCharacter(character: Character) {
+        this.character = character
+        headerText.text = character.name
+        loadHomeImage(imageView, character.thumbnailUrl, progress)
+        favoriteCheckBox.isChecked = character.favorite
+    }
+
+    fun applyDetailMode() {
+        imageView.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        imageView.setPadding(0, 0, 0, 0)
+    }
+
+    fun setListeners(favoriteClick: (Character) -> Unit, itemClick: ((Character) -> Unit)? = null) {
+        this.favoriteClick = favoriteClick
+        this.itemClick = itemClick
+    }
+}
