5c407ea98cf0dde31e6bd8a1acce9a0c2b7993b8
Thiago Souto <soutosss@gmail.com>
2022-01-16 17:58:57 +0100

    Add AndroidJUnit4 annotation

--- app/src/androidTest/kotlin/com/soutosss/marvelpoc/home/list/FavoriteFragmentTest.kt
+++ app/src/androidTest/kotlin/com/soutosss/marvelpoc/home/list/FavoriteFragmentTest.kt
@@ -1,7 +1,10 @@
 package com.soutosss.marvelpoc.home.list
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class FavoriteFragmentTest {
 
     @Test
