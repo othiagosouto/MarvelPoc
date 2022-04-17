cfa1dd70634a50eb21fd9380d102797cb1b292c6
Thiago Souto <soutosss@gmail.com>
2022-04-17 15:58:31 +0200

    Use dimens from the design system module

--- app/src/main/kotlin/com/soutosss/marvelpoc/detail/CharacterDetails.kt
+++ app/src/main/kotlin/com/soutosss/marvelpoc/detail/CharacterDetails.kt
@@ -2,7 +2,17 @@ package com.soutosss.marvelpoc.detail
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.CircularProgressIndicator
@@ -13,12 +23,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.ImagePainter
 import coil.compose.rememberImagePainter
 import com.soutosss.marvelpoc.R
-import com.soutosss.marvelpoc.data.CharacterDetails
+import dev.thiagosouto.marvelpoc.design.R as RDesign
 
 @Composable
 fun CharacterDetails(name: String, description: String, imageUrl: String) {
@@ -68,8 +79,8 @@ fun CharacterDetails(name: String, description: String, imageUrl: String) {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(
-                    horizontal = 16.dp,
-                    vertical = 8.dp
+                    horizontal = dimensionResource(id = RDesign.dimen.spacing_medium),
+                    vertical = dimensionResource(id = RDesign.dimen.spacing_small)
                 )
                 .testTag("description"),
             text = description.ifBlank { stringResource(id = R.string.character_details_description_not_available) })
@@ -83,6 +94,6 @@ fun Loading() {
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        CircularProgressIndicator(modifier = Modifier.size(72.dp))
+        CircularProgressIndicator(modifier = Modifier.size(dimensionResource(id = RDesign.dimen.loading_page_size)))
     }
 }
