9f20c0b09d435b62f9d7e4adf3bc5d99a69a5774
Thiago Souto <soutosss@gmail.com>
2022-05-26 17:08:38 -0300

    Promote FavoriteToggle to design module

--- app/src/main/kotlin/com/soutosss/marvelpoc/widget/CharacterItem.kt
+++ app/src/main/kotlin/com/soutosss/marvelpoc/widget/CharacterItem.kt
@@ -1,10 +1,6 @@
 package com.soutosss.marvelpoc.widget
 
 import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
-import androidx.compose.animation.graphics.res.animatedVectorResource
-import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
-import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -19,13 +15,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.IconToggleButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -40,6 +31,7 @@ import coil.compose.ImagePainter
 import coil.compose.rememberImagePainter
 import com.soutosss.marvelpoc.data.model.view.Character
 import dev.thiagosouto.marvelpoc.design.R
+import dev.thiagosouto.marvelpoc.design.components.FavoriteToggle
 
 @Composable
 fun CharacterItem(
@@ -82,32 +74,6 @@ fun CharacterItem(
                 onCheckedChange = onCheckedChange
             )
         }
-    }
-}
-
-@OptIn(ExperimentalAnimationGraphicsApi::class)
-@Composable
-private fun FavoriteToggle(
-    isFavorite: Boolean,
-    onCheckedChange: (Boolean) -> Unit
-) {
-    var atEnd by remember { mutableStateOf(isFavorite) }
-    val image = AnimatedImageVector.animatedVectorResource(id = R.drawable.heart_animation)
-    val painter = rememberAnimatedVectorPainter(
-        image,
-        atEnd
-    )
-    IconToggleButton(
-        checked = isFavorite,
-        onCheckedChange = {
-            atEnd = it
-            onCheckedChange(it)
-        }
-    ) {
-        Image(
-            painter = painter,
-            contentDescription = "",
-        )
     }
 }
 
---
+++ design/src/main/kotlin/dev/thiagosouto/marvelpoc/design/components/FavoriteToggle.kt
@@ -0,0 +1,41 @@
+package dev.thiagosouto.marvelpoc.design.components
+
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
+import androidx.compose.foundation.Image
+import androidx.compose.material.IconToggleButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import dev.thiagosouto.marvelpoc.design.R
+
+
+@OptIn(ExperimentalAnimationGraphicsApi::class)
+@Composable
+fun FavoriteToggle(
+    isFavorite: Boolean,
+    onCheckedChange: (Boolean) -> Unit
+) {
+    var atEnd by remember { mutableStateOf(isFavorite) }
+    val image = AnimatedImageVector.animatedVectorResource(id = R.drawable.heart_animation)
+    val painter = rememberAnimatedVectorPainter(
+        image,
+        atEnd
+    )
+    IconToggleButton(
+        checked = isFavorite,
+        onCheckedChange = {
+            atEnd = it
+            onCheckedChange(it)
+        }
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = "",
+        )
+    }
+}
