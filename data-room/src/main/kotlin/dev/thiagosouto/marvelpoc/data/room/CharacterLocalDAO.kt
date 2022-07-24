64d369b1066ef7a188f50fb955fbe895bb1aa17d
Thiago Souto <soutosss@gmail.com>
2022-07-24 19:19:10 +0200

    Create method to retrieve PagingSource from CharacterLocalDAO

--- data-room/src/main/kotlin/dev/thiagosouto/marvelpoc/data/room/CharacterLocalDAO.kt
+++ data-room/src/main/kotlin/dev/thiagosouto/marvelpoc/data/room/CharacterLocalDAO.kt
@@ -1,6 +1,7 @@
 package dev.thiagosouto.marvelpoc.data.room
 
 import androidx.paging.DataSource
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
@@ -9,6 +10,9 @@ import androidx.room.Query
 
 @Dao
 internal interface CharacterLocalDAO {
+
+    @Query("SELECT * FROM ${CharacterLocal.TABLE_NAME}")
+    fun favoritesList(): PagingSource<Int, CharacterLocal>
 
     @Query("SELECT * FROM ${CharacterLocal.TABLE_NAME}")
     fun favoriteList(): DataSource.Factory<Int, CharacterLocal>
