965f5566faf4e438f92b86969697946bdf81ea7f
Thiago Souto <soutosss@gmail.com>
2023-08-04 18:37:42 +0200

    Extract webserver to an external lib

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,6 +3,9 @@ import org.gradle.kotlin.dsl.`kotlin-dsl`
 repositories {
     google()
     mavenCentral()
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+    }
 }
 
 plugins {
