3a76db4e3eb7af40f7671ec0cc9d43abb25281e6
Thiago Souto <soutosss@gmail.com>
2023-11-12 22:58:48 +0100

    Remove mockk usage from CharactersRepositoryImplTest

--- data/src/test/kotlin/dev/thiagosouto/marvelpoc/data/CharactersRepositoryImplTest.kt
+++ data/src/test/kotlin/dev/thiagosouto/marvelpoc/data/CharactersRepositoryImplTest.kt
@@ -1,84 +1,121 @@
 package dev.thiagosouto.marvelpoc.data
 
+import androidx.paging.DataSource
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
 import com.google.common.truth.Truth.assertThat
 import dev.thiagosouto.marvelpoc.data.character.CharacterDetailsRemoteContract
 import dev.thiagosouto.marvelpoc.data.character.CharacterLocalContract
 import dev.thiagosouto.marvelpoc.data.character.CharacterRemoteContract
 import dev.thiagosouto.marvelpoc.data.model.view.Character
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.test.runTest
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import java.lang.Exception
 
 @OptIn(ExperimentalCoroutinesApi::class)
 internal class CharactersRepositoryImplTest {
 
-    private lateinit var remoteSourceMock: CharacterRemoteContract<Character>
-    private lateinit var localSourceMock: CharacterLocalContract<Character>
-    private lateinit var characterDetailsRemoteContract: CharacterDetailsRemoteContract<CharacterDetails>
-    private lateinit var repository: CharactersRepositoryImpl
+    private val remoteSourceMock = FakeRemote()
+    private val localSourceMock = FakeLocal()
+    private val characterDetailsRemoteContract: CharacterDetailsRemoteContract<CharacterDetails> =
+        FakeDetails()
+    private val repository: CharactersRepositoryImpl = CharactersRepositoryImpl(
+        localSourceMock,
+        remoteSourceMock,
+        characterDetailsRemoteContract
+    )
     private lateinit var item: Character
 
     @Before
     fun setup() {
-        remoteSourceMock = mockk(relaxed = true)
-        localSourceMock = mockk(relaxed = true)
-        characterDetailsRemoteContract = mockk()
-        repository =
-            CharactersRepositoryImpl(
-                localSourceMock,
-                remoteSourceMock,
-                characterDetailsRemoteContract
-            )
         item = Character(1011334, "some name", "some url", "description", true)
     }
 
-    @Test
+    @After
+    fun tearDown() {
+        localSourceMock.favoriteIds.clear()
+    }
+
+    @Test(expected = FakeLocal.FavoriteListCalledException::class)
     fun `fetchFavoriteCharacters should call dao to retrieve all favorite items`() = runTest {
         repository.fetchFavoriteCharacters()
 
-        coVerify(exactly = 1) { localSourceMock.favoriteList() }
     }
 
     @Test
     fun `favoriteCharacter should call dao to insert favorite item`() = runTest {
         repository.favorite(item)
 
-        coVerify(exactly = 1) { localSourceMock.favorite(item) }
+        assertThat(localSourceMock.favoriteIds).isNotEqualTo(listOf(item.id))
     }
 
     @Test
     fun `unFavoriteCharacter should call dao to delete item`() = runTest {
         val parameter = item.copy(favorite = false)
-        coEvery { localSourceMock.unFavorite(parameter) } returns parameter.id
+        repository.favorite(item)
 
         repository.unFavorite(parameter)
 
-        coVerify(exactly = 1) { localSourceMock.unFavorite(parameter) }
-    }
-
-    @Test
-    fun `charactersDataSource should call listCharacters with expected parameters `() {
-        repository.charactersPagingDataSource(null, 10)
-
-        coVerify {
-            remoteSourceMock.listPagingCharacters(
-                null,
-                10,
-                localSourceMock::favoriteIds
-            )
-        }
+      assertThat(localSourceMock.favoriteIds).isEmpty()
     }
 
     @Test
     fun `fetchFavoriteIds return a list of favorite ids`() = runTest {
         val ids: List<Long> = listOf(1, 2, 3)
-        coEvery { localSourceMock.favoriteIds() } returns ids
+        localSourceMock.favoriteIds.addAll(ids)
 
         assertThat(repository.fetchFavoriteIds()).isEqualTo(ids)
+    }
+}
+
+class FakeRemote : CharacterRemoteContract<Character> {
+    override fun listPagingCharacters(
+        queryText: String?,
+        pageSize: Int,
+        provideFavoriteIds: suspend () -> List<Long>
+    ): PagingSource<Int, Character> {
+        TODO("Not yet implemented")
+    }
+}
+
+
+class FakeLocal : CharacterLocalContract<Character> {
+
+    val favoriteIds = mutableSetOf<Long>()
+    override fun favoritesList(pageSize: Int, maxSize: Int): Flow<PagingData<Character>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun favoriteList(): DataSource.Factory<Int, Character> {
+       throw FavoriteListCalledException()
+    }
+
+    override suspend fun favoriteIds(): List<Long> = favoriteIds.toList()
+
+    override suspend fun unFavorite(item: Character): Long {
+        favoriteIds.remove(item.id)
+        return item.id
+    }
+
+    override suspend fun favorite(item: Character): Long {
+        favoriteIds.add(item.id)
+        return item.id
+    }
+
+    class FavoriteListCalledException : Exception()
+}
+
+class FakeDetails : CharacterDetailsRemoteContract<CharacterDetails> {
+    override suspend fun fetchCharacterDetails(characterId: String): CharacterDetails {
+        return CharacterDetails(
+            id = characterId.toLong(),
+            name = "name",
+            description = "description",
+            imageUrl = "",
+            comics = emptyList()
+        )
     }
 }
