ce37b2d95f6fa191164da550e4c1f54fb4e6cbea
Thiago Souto <soutosss@gmail.com>
2023-12-03 23:31:31 +0100

    Add coroutines to domain module

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -26,6 +26,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             //put your multiplatform dependencies here
+            implementation(libs.kotlin.coroutines)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
