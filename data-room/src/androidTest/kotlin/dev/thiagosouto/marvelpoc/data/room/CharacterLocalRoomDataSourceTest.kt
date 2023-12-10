6ff57277735596eb46afd97258210d5f24cdb3c1
Thiago Souto <soutosss@gmail.com>
2023-12-11 00:23:35 +0100

    Fix rom integrated tests

--- data-room/src/androidTest/kotlin/dev/thiagosouto/marvelpoc/data/room/CharacterLocalRoomDataSourceTest.kt
+++ data-room/src/androidTest/kotlin/dev/thiagosouto/marvelpoc/data/room/CharacterLocalRoomDataSourceTest.kt
@@ -8,7 +8,6 @@ import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
 import dev.thiagosouto.marvelpoc.data.room.ext.toCharacter
 import dev.thiagosouto.marvelpoc.domain.model.Character
-import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
@@ -39,9 +38,6 @@ internal class CharacterLocalRoomDataSourceTest {
         )
 
     @get:Rule
-    var coroutineTestRule = CoroutineTestRule()
-
-    @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
 
     @Before
@@ -53,60 +49,59 @@ internal class CharacterLocalRoomDataSourceTest {
     }
 
     @Test
-    fun favorite_shouldInsertTheExpectedItemToFavoriteAndReturnItsId() =
-        coroutineTestRule.testDispatcher.runBlockingTest {
-            val character =
-                Character(
-                    30,
-                    NAME,
-                    URL,
-                    DESCRIPTION,
-                    true
-                )
-            assertThat(dataSource.favorite(character)).isEqualTo(30)
+    fun favorite_shouldInsertTheExpectedItemToFavorite() = runTest {
+        dataSource.favorite(character)
+
+        dataSource.favoriteIds().test {
+            assertThat(awaitItem()).contains(30L)
         }
+    }
 
     @Test
-    fun unFavorite_shouldUnFavoriteAndReturnsTheIdOfItemRemoved() =
-        coroutineTestRule.testDispatcher.runBlockingTest {
-            characterLocalDAO.favorite(characterLocal)
-            assertThat(dataSource.unFavorite(character)).isEqualTo(30)
+    fun unFavorite_shouldUnFavoriteAndReturns() = runTest {
+        dataSource.favorite(character)
+        dataSource.unFavorite(character)
+
+        dataSource.favoriteIds().test {
+            assertThat(awaitItem()).doesNotContain(30L)
         }
+    }
 
     @Test
-    fun favoriteIds_shouldReturnAllIdsOfFavoriteItems() =
-        coroutineTestRule.testDispatcher.runBlockingTest {
-            characterLocalDAO.favorite(characterLocal.copy(id = 1))
-            characterLocalDAO.favorite(characterLocal.copy(id = 2))
-            characterLocalDAO.favorite(characterLocal.copy(id = 3))
+    fun favoriteIds_shouldReturnAllIdsOfFavoriteItems() = runTest {
+        characterLocalDAO.favorite(characterLocal.copy(id = 1))
+        characterLocalDAO.favorite(characterLocal.copy(id = 2))
+        characterLocalDAO.favorite(characterLocal.copy(id = 3))
 
-            dataSource.favoriteIds().test{
+        dataSource.favoriteIds().test {
 
-                assertThat(awaitItem()).isEqualTo(listOf<Long>(1, 2, 3))
-            }
+            assertThat(awaitItem()).isEqualTo(listOf<Long>(1, 2, 3))
         }
+    }
 
     @Test
     fun favoriteList_shouldReturnFavoriteList() = runTest {
-            val character1 = characterLocal.copy(id = 1)
-            val character2 = characterLocal.copy(id = 2)
-            val character3 = characterLocal.copy(id = 3)
+        val character1 = characterLocal.copy(id = 1)
+        val character2 = characterLocal.copy(id = 2)
+        val character3 = characterLocal.copy(id = 3)
 
-            characterLocalDAO.favorite(character1)
-            characterLocalDAO.favorite(character2)
-            characterLocalDAO.favorite(character3)
+        characterLocalDAO.favorite(character1)
+        characterLocalDAO.favorite(character2)
+        characterLocalDAO.favorite(character3)
 
-           dataSource.favoritesList().test{
+        dataSource.favoritesList().test {
 
-               assertThat(awaitItem()).isEqualTo(listOf(
-                   character1.toCharacter(),
-                   character2.toCharacter(),
-                   character3.toCharacter()
-               ))
-           }
+            assertThat(awaitItem()).isEqualTo(
+                listOf(
+                    character1.toCharacter(),
+                    character2.toCharacter(),
+                    character3.toCharacter()
+                )
+            )
         }
+    }
 
-    private companion object Mock{
+    private companion object Mock {
         const val DESCRIPTION = "description"
         const val NAME = "name"
         const val URL = "url"
