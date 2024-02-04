a8d1dfefcec259bddcc8d03c437ebc7fbd5bc752
Thiago Souto <thiago.santos@hellofresh.com>
2024-02-04 15:44:37 +0100

    Create dimens file

---
+++ design/src/commonMain/kotlin/dev/thiagosouto/marvelpoc/design/dimens/Dimens.kt
@@ -0,0 +1,10 @@
+package dev.thiagosouto.marvelpoc.design.dimens
+
+import androidx.compose.ui.unit.dp
+
+object Dimens{
+    object Spacing {
+        val small = 8.dp
+        val medium = 16.dp
+    }
+}
